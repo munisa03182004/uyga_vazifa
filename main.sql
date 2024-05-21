1d037545f8b39f3c35afe08875de7e71a3275325
Munisa <munisanormuhammedova@gmil.com>
2024-05-21 08:11:43 +0500

    first commit

---
+++ main.sql
@@ -0,0 +1,21 @@
+-- 1-masala 
+select sum(purch_amt) from orders;
+
+-- 2-masala
+select avg(purch_amt) from orders;
+
+-- 3-masala
+select COUNT(DISTINCT salesman_id) from orders;
+
+-- 4-masala
+select  count(*) from customer:
+
+-- 5-masala
+select count(all grade) from customer;
+
+-- 6-masala
+select max(purch_amt) from orders;
+
+-- 7-masala
+select min(purch_amt) from orders;
+
